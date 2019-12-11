20375165ce68a3d1262bc89574f1eef7e03f7141
Rahul <35rahuldshetty@gmail.com>
2019-12-11 22:09:48 +0530

    added few tiles

--- Assets/FollowScript.cs
+++ Assets/FollowScript.cs
@@ -10,6 +10,6 @@ public class FollowScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.position.x,transform.position.y,-10f); 
+        transform.position = new Vector3(target.position.x, target.position.y,-10f); 
     }
 }
---
+++ Assets/PositionSortRendered.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionSortRendered : MonoBehaviour
+{
+    [SerializeField]
+    private int sortingOrderBase = 5000;
+    [SerializeField]
+    private int offset = 0;
+
+    [SerializeField]
+    private bool runOnlyOnce = false;
+
+    private float timer,timerMax = .1f;
+
+
+    private Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        timer -= Time.deltaTime;
+        if(timer<=0f)
+        {
+            timer = timerMax;
+            renderer.sortingOrder = (int)(sortingOrderBase - transform.position.y - offset);
+            if (runOnlyOnce)
+            {
+                Destroy(this);
+            }
+        }
+        
+    }
+}
